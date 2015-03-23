508aefd2a7235f0d86a1b2432601013ffb9cbf23
Duy Buffet <Duy Buffet@Buffet_probook>
2015-03-24 01:23:19 +0700

    Lam viec voi rmi

---
+++ UIDModel/src/remote/RMICitizenAction.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package remote;
+
+import common.model.PersonDetails;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Duy Buffet
+ */
+public interface RMICitizenAction extends Remote {
+
+    ArrayList<PersonDetails> search(String fName, String mName, String lName) throws RemoteException;
+
+    void sendRequest(PersonDetails person, int centre) throws RemoteException;
+}
